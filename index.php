b3e4ff3bf9a8f3e9d2e236704bca368eb843d595
duongcongit <duongcongit@gmail.com>
2022-03-11 15:00:44 +0700

    update branch phamvanphu for merge to main

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-  session_start();
-  require 'dbConfig.php';
+  require './config/constants.php';
   
   $sql = "SELECT * FROM products
   order by productID desc";
@@ -22,7 +21,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
 </head>
 
 <body>
