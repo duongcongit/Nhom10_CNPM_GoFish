47869812f3a3a4138e16634befe6096686ec8ea2
serifinal <84276150+serifinal@users.noreply.github.com>
2022-03-14 16:26:08 +0700

    upload

---
+++ seller/products/delete-product.php
@@ -0,0 +1,168 @@
+<?php
+include "../../config/constants.php";
+
+include "../partials/header.php";
+
+?>
+
+<!-- Content start-->
+
+<div class="col-md-12 mt-2 mb-3 nav-page">
+    <h5 class="text-muted"><a href="">Kênh người bán</a> / <a href="">Quản lý sản phẩm</a> / <b href="">Xóa sản phẩm</b></h5>
+</div>
+
+<!--  -->
+<div class="alert alert-success alert-dismissible alert-update-success d-flex align-items-center d-none" role="alert">
+    <i class="bi bi-check-circle-fill me-2" width="24" height="24"></i>
+    <div>
+        <p id="alert-success-content" class="d-inline"></p><strong id="alert-success-taget"></strong><span> thành công!</span>
+    </div>
+    <button type="button" class="btn-close" onclick="removeAlert()"></button>
+    <script>
+        function removeAlert() {
+            $(".alert-update-success").addClass("d-none");
+        }
+    </script>
+</div>
+
+<!--  -->
+
+<div class="alert alert-success alert-dismissible d-flex align-items-center 
+<?php
+if (!isset($_SESSION['editProdSucsess']) && !isset($_SESSION['addProdSucsess']) && !isset($_SESSION['upProdStockSucsess'])) {
+    echo "d-none";
+}
+?>
+ " id="alert-success-pr" role="alert">
+    <i class="bi bi-check-circle-fill me-2" width="24" height="24"></i>
+    <div>
+        <p class="d-inline">
+            <?php
+            if (isset($_SESSION['addProdSucsess'])) {
+                echo $_SESSION['addProdSucsess'];
+                unset($_SESSION['addProdSucsess']);
+            }
+            if (isset($_SESSION['editProdSucsess'])) {
+                echo $_SESSION['editProdSucsess'];
+                unset($_SESSION['editProdSucsess']);
+            }
+            ?>
+        </p>
+    </div>
+    <button type="button" class="btn-close" onclick="rmAlert()"></button>
+    <script>
+        function rmAlert() {
+            $("#alert-success-pr").addClass("d-none");
+        }
+    </script>
+</div>
+
+<!--  -->
+
+<div class="col-md-12 manage-products shadow">
+    <div class="col-md-12 mt-3">
+        <div class="d-flex justify-content-between">
+            <?php
+            //Sql Query 
+            $sql_products = "SELECT * FROM products WHERE userID = '{$_SESSION['userID']}'";
+            //Execute Query
+            $result = $conn->query($sql_products);
+            
+            //Count Rows
+            $count_products = $result->num_rows;
+
+
+            ?>
+            <h3 class="ms-2" id="label-count-prod"><?php echo $count_products ?> Sản phẩm </h3>
+        </div>
+        <!--  -->
+        <table class="styled-table">
+            <thead>
+                <tr>
+                    <th>Tên sản phẩm</th>
+                    <th>Mã Sản Phẩm</th>
+                    <th>Danh mục hàng</th>
+                    <th>Giá</th>
+                    <th>Kho hàng</th>
+                    <th>Đã bán</th>
+                    <th>Xóa sản phẩm</th>
+                </tr>
+            </thead>
+            <tbody id="table-products">
+                <!--  -->
+                <?php
+                if($count_products > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                    ?>
+                        <tr>
+                            <td class="row">
+                                <div style="max-width: fit-content;">
+                                    <img src="../../assets/img/products/<?php echo explode(",", $row['image'])[0]; ?>" alt="" class="product-avatar-list">
+                                </div>
+                                <div class="col row d-flex align-items-center">
+                                    <?php
+                                    if ($row['status'] == 1 and $row['stock'] > 0) {
+                                    ?>
+                                        <div class="col-md-12 produc-status-active">
+                                            Đang hoạt động
+                                        </div>
+                                    <?php
+                                    }
+                                    if ($row['status'] == 1 and $row['stock'] == 0) {
+                                    ?>
+                                        <div class="col-md-12 produc-status-out">
+                                            Hết hàng
+                                        </div>
+                                    <?php
+                                    }
+                                    if ($row['status'] == 3) {
+                                    ?>
+                                        <div class="col-md-12 produc-status-locked">
+                                            Bị khóa
+                                        </div>
+
+                                    <?php
+                                    }
+                                    ?>
+                                    <div class="col-md-12">
+                                        <b><?= $row['productName']; ?></b>
+                                    </div>
+                                </div>
+                            </td>
+                            <td><?= $row['productSKU']; ?></td>
+                            <td><?= $row['category']; ?></td>
+                            <td><?= $row['price']; ?>đ</td>
+                            <td class="text-center"><?= $row['stock']; ?>
+                            </td>
+                            <td class="text-center"><?= $row['sold']; ?></td>
+                            <td class="text-center">
+                                <a href="./delete-product.php?productid=<?php echo $row['productID'];?>" class="bi bi-pencil-square fs-5"></a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                }else{
+                    ?>
+                    <tr>
+                        <td colspan="7">
+                            <div class="mt-5 mb-5 col-md-12 h-50 d-flex flex-column justify-content-center align-items-center">
+                                <i class="bi bi-search text-muted" style="font-size: 40px;"></i>
+                                <p class="text-muted fs-5">Tài khoản hiện chưa đăng sản phẩm nào</p>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php
+                    }
+                ?>
+                <!--  -->
+            </tbody>
+        </table>
+        <!--  -->
+    </div>
+
+</div>
+<!-- Content end-->
+
+<?php
+include "../partials/footer.php";
+?>
---
+++ seller/products/get-products-data.php
@@ -0,0 +1,132 @@
+<?php
+include "../../config/constants.php";
+
+if (isset($_POST['tableID'])) {
+    $tableType = $_POST['type'];
+    $page = $_POST['page'];
+    $numRow = $_POST['numRow'];
+    $sortBy = $_POST['sortBy'];
+    $productStatus = "";
+
+    if ($tableType == "all") {
+        $productStatus = "";
+    } else if ($tableType == "active") {
+        $productStatus = "status=1 and stock>0 and";
+    } else if ($tableType == "run_out") {
+        $productStatus = "stock=0 and";
+    } else if ($tableType == "locked") {
+        $productStatus = "status=3 and";
+    }
+
+
+    $sqlTotalRow    = "SELECT * FROM products WHERE $productStatus userID = '{$_SESSION['userID']}'";
+    $totalRow       = $conn->query($sqlTotalRow)->num_rows;
+    $numPage        = ceil($totalRow / $numRow);
+    $start          = ($page - 1) * $numRow;
+    $sql            = $sqlTotalRow . " ORDER BY $sortBy" . " LIMIT $start, $numRow";
+    $result         = $conn->query($sql);
+
+
+    if ($totalRow > 0) {
+?>
+        <?php
+        $i = $start;
+        while ($row = $result->fetch_assoc()) {
+        ?>
+            <tr>
+                <td class="row">
+                    <div style="max-width: fit-content;">
+                        <img src="../../assets/img/products/<?php echo explode(",", $row['image'])[0]; ?>" alt="" class="product-avatar-list">
+                    </div>
+                    <div class="col row d-flex align-items-center">
+                        <?php
+                        if ($row['status'] == 1 and $row['stock'] > 0) {
+                        ?>
+                            <div class="col-md-12 produc-status-active">
+                                Đang hoạt động
+                            </div>
+                        <?php
+                        }
+                        if ($row['status'] == 1 and $row['stock'] == 0) {
+                        ?>
+                            <div class="col-md-12 produc-status-out">
+                                Hết hàng
+                            </div>
+                        <?php
+                        }
+                        if ($row['status'] == 3) {
+                        ?>
+                            <div class="col-md-12 produc-status-locked">
+                                Bị khóa
+                            </div>
+
+                        <?php
+                        }
+                        ?>
+                        <div class="col-md-12">
+                            <b><?= $row['productName']; ?></b>
+                        </div>
+                    </div>
+                </td>
+                <td><?= $row['productSKU']; ?></td>
+                <td><?= $row['category']; ?></td>
+                <td><?= $row['price']; ?>đ</td>
+                <td class="text-center"><?= $row['stock']; ?>
+                    <?php
+                    if ($row['status'] == 3) {
+                    ?>
+                        <i class="bi bi-pencil-fill fs-6 text-muted" style="cursor:not-allowed;"></i>
+
+                    <?php
+                    } else {
+                    ?>
+                        <i class="bi bi-pencil-fill text-danger fs-6 btn-update-stock" type="button" data-productid="<?php echo $row['productID']; ?>" data-productstock="<?php echo $row['stock']; ?>" data-productsku="<?php echo $row['productSKU']; ?>"></i>
+                    <?php
+                    }
+                    ?>
+                </td>
+                <td class="text-center"><?= $row['sold']; ?></td>
+                <td class="text-center">
+                    <?php
+                    if ($row['status'] == 3) {
+                    ?>
+                        <i class="bi bi-pencil-square fs-5 text-muted" style="cursor:not-allowed;"></i>
+
+                    <?php
+                    } else {
+                    ?>
+                        <a href="./edit-product.php?productid=<?php echo $row['productID']; ?>&productsku=<?php echo $row['productSKU']; ?>" class="bi bi-pencil-square fs-5"></a>
+                    <?php
+                    }
+                    ?>
+
+                </td>
+            </tr>
+        <?php
+        }
+    } else {
+        ?>
+        <tr>
+            <td colspan="7">
+                <div class="mt-5 mb-5 col-md-12 h-50 d-flex flex-column justify-content-center align-items-center">
+                    <i class="bi bi-search text-muted" style="font-size: 40px;"></i>
+                    <p class="text-muted fs-5">Không tìm thấy dữ liệu</p>
+                </div>
+            </td>
+        </tr>
+
+
+<?php
+    }
+    //
+
+
+
+    //
+}
+else {
+    header("location:" . SITEURL . "seller/");
+}
+
+
+?>
