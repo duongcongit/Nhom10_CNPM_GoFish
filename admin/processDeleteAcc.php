a2bc22a38dc0cc150c14fa79d127581f507a7707
pvp2105 <94332470+pvp2105@users.noreply.github.com>
2022-03-18 15:40:35 +0700

    Update processDeleteAcc.php

--- admin/processDeleteAcc.php
+++ admin/processDeleteAcc.php
@@ -3,9 +3,11 @@
     $manguoidung = $_GET['id'];
         $sql = "Delete from users where id = '$manguoidung'  ";
         $number = mysqli_query($conn,$sql);
-        $sql2 = "Delete from products where userid = '$manguoidung'  ";
+        $sql2 = "Delete from products where userID = '$manguoidung'  ";
         $number2 = mysqli_query($conn,$sql2);
-        if($number > 0 or $number2 > 0){
+        $sql3 = "Delete from cart where userID = '$manguoidung'  ";
+        $number3 = mysqli_query($conn,$sql3);
+        if($number > 0 or $number2 > 0 or $number3 > 0){
             header("location: account.php"); //Chuyển hướng về trang quản trị
         }
         else{
