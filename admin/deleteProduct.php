79f034d65c82d4b0cb8347c347fad24a4c8b0e4c
xuanbinh2721 <binhphan2721@gmail.com>
2022-03-18 12:08:29 +0700

    update delete product

--- admin/deleteProduct.php
+++ admin/deleteProduct.php
@@ -8,12 +8,16 @@
         $res = mysqli_query($conn,$sql);
         $sql1="DELETE FROM products WHERE productID='$id'";
         $res1 = mysqli_query($conn,$sql1);
+        $sql2="DELETE FROM cart WHERE productID='$id'";
+        $res2 = mysqli_query($conn,$sql2);
         
-        if($res >0){
-            if($res1 >0){
-                $_SESSION['success'] = 'Xóa sản phẩm thành công!';
-                header("location: products.php"); 
-                exit();
+        if($res >0 ){
+            if($res2 >0){
+                if($res1 >0){
+                    $_SESSION['success'] = 'Xóa sản phẩm thành công!';
+                    header("location: products.php"); 
+                    exit();
+                }
             }
             else{
                 $_SESSION['error'] = 'Có lỗi';
