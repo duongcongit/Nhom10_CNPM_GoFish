d91dc1f71e928be0b136dbb0854e451dc1daefca
pvp2105 <94332470+pvp2105@users.noreply.github.com>
2022-03-18 15:41:43 +0700

    Update processDeleteAllAcc.php

--- admin/processDeleteAllAcc.php
+++ admin/processDeleteAllAcc.php
@@ -4,7 +4,9 @@
         $number = mysqli_query($conn,$sql);
         $sql2 = "Delete from products";
         $number2 = mysqli_query($conn,$sql2);
-        if($number > 0 or $number2 > 0){
+        $sql3 = "Delete from cart";
+        $number3 = mysqli_query($conn,$sql3);
+        if($number > 0 or  $number2 > 0 or $number3 > 0 ){
             header("location: account.php"); //Chuyển hướng về trang quản trị
         }
         else{
